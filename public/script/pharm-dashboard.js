a12cff3037e2391232f6b1147087608daff93741
Eric C <39015447+ricetech@users.noreply.github.com>
2020-02-09 05:52:08 -0500

    Added table rendering function

--- public/script/pharm-dashboard.js
+++ public/script/pharm-dashboard.js
@@ -1,6 +1,66 @@
+function renderTable() {
+    let db = firebase.firestore();
+    let drugs = [];
+    db.collection("drugs").get().then((querySnapshot) => {
+
+        let table = document.getElementById("pharm-table");
+        querySnapshot.forEach((doc) => {
+            let data = doc.data();
+            let dataWithID = Object.assign({id:doc.id}, data);
+            drugs.push(dataWithID);
+        });
+        console.log(drugs);
+        let drug;
+        for (drug of drugs) {
+            let cells = [];
+            let row = table.insertRow();
+            let i;
+            for (i = 0; i < 9, i++;) {
+                cells[i] = row.insertCell(i);
+            }
+            let keys = Object.keys(drug);
+            let key;
+            let index;
+            for (key of keys) {
+                switch(key) {
+                    case "id":
+                        index = 0;
+                        break;
+                    case "trade_name":
+                        index = 1;
+                        break;
+                    case "generic_name":
+                        index = 2;
+                        break;
+                    case "type":
+                        index = 3;
+                        break;
+                    case "variant":
+                        index = 4;
+                        break;
+                    case "amount":
+                        index = 5;
+                        break;
+                    case "q_ott":
+                        index = 6;
+                        break;
+                    case "q_tor":
+                        index = 7;
+                        break;
+                    case "price":
+                        index = 8;
+                }
+                cells[index].innerHTML = drug[key];
+            }
+
+        }
+    });
+}
 $(document).ready(function () {
     firebase.auth().onAuthStateChanged(function (user) {
-        if (!user) {
+        if (user) {
+            renderTable();
+        } else {
             window.location.href = 'index.html';
         }
     });
--- public/script/script.js
+++ public/script/script.js
@@ -28,34 +28,4 @@ $(document).ready(function () {
         console.log(drugs);
 
     });
-
-    try {
-        let app = firebase.app();
-        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-        document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
-    } catch (e) {
-        console.error(e);
-        document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-    }
-// Firebase test script
-//     document.addEventListener('DOMContentLoaded', function () {
-//         //
-//         // // The Firebase SDK is initialized and available here!
-//         //
-//         // firebase.auth().onAuthStateChanged(user => { });
-//         // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-//         // firebase.messaging().requestPermission().then(() => { });
-//         // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-//         //
-//         //
-//         console.log(firebase)
-//         try {
-//             let app = firebase.app();
-//             let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-//             document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
-//         } catch (e) {
-//             console.error(e);
-//             document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-//         }
-//     });
 });
